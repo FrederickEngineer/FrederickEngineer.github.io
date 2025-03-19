63d10e7429797647abe6e885edddfb5a57832e76
Frederick <praivateroom@gmail.com>
2025-03-19 13:16:21 +0900

    main

---
+++ Sneakers/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    // カールセルスライダー
+    $(".slide-items").slick({
+        slidesToScroll:1,
+        slidesToShow:3,
+        centerMode:true,
+        centerPadding: '10%',
+        variableWidth: true
+    });
+
+});
