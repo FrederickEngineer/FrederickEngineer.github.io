84edc38ddb4ccedfc46c1ecf4407f06edada762e
Frederick <praivateroom@gmail.com>
2025-03-27 22:25:21 +0900

    main

--- Sneakers/js/script.js
+++ Sneakers/js/script.js
@@ -1,12 +1,36 @@
-$(function(){
+$(function () {
 
     // カールセルスライダー
     $(".slide-items").slick({
-        slidesToScroll:1,
-        slidesToShow:3,
-        centerMode:true,
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        centerMode: true,
         centerPadding: '10%',
         variableWidth: true
     });
 
+
+    // トグルメニューON
+    $("#toggle-btn").click(function () {
+        $("#Menu").toggleClass("open");
+        $("#global-nav").toggleClass("open");
+        $("#toggle-btn span").toggleClass("open");
+    });
+
+    // スクロールイベント
+    $(window).scroll(function () {
+        // スクロール量を取得
+        const scroll = $(window).scrollTop();
+        // 画面の高さを取得
+        const windowHeight = $(window).height();
+
+        $(".Feature-grid-child img").each(function() {
+            // それぞれの.boxまでの高さを取得
+            const boxHeight = $(this).offset().top;
+            // 条件式に合致する場合はis-activeを付与
+            if(scroll + 600 > boxHeight) {
+              $(this).addClass("is-active");
+            }
+          });
+    });
 });
